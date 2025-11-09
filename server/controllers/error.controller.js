899f6166c4842e4d3f59b4d625bf600e8367dcc9
Quinn <89018063+kqfall1@users.noreply.github.com>
2025-11-08 22:21:50 -0500

    Altered error.controller.js
    Made most documentation more professional.

--- server/controllers/error.controller.js
+++ server/controllers/error.controller.js
@@ -1,42 +1,36 @@
 import dbErrorHandler from "../helpers/dbErrorHandler.js";
 
-/*
- * Middleware function to handle errors passed through the request object.
- * It captures the error, formats a user-friendly error message,
- * determines the appropriate HTTP status code, logs the error for debugging,
- * and sends a JSON response to the client.
- *
- * req - The Express request object, expected to have an 'error' property.
- * res - The Express response object.
- */
-
+/**
+* Handles errors passed through the request object by capturing the error, formatting a 
+* user-friendly error message, determining the appropriate HTTP status code, logging the 
+* error, and sending a JSON response to the client.
+* @param req The Express request object, expected to have an 'error' property.
+* @param res The Express response object.
+*/
 function handleError(req, res) {
-  const err = req.error; //capture error from request object
-
+  const err = req.error; 
   const errorMessage = dbErrorHandler.getErrorMessage(err);
-  const statusCode = err.status || 500; // set status code or default to 500 if no status is set
+  const statusCode = err.status || 500; 
 
   console.log(err);
 
-  // Send the error response as JSON
   res.status(statusCode).json({
     error: errorMessage,
   });
 }
 
-/*
- * Generates a formatted error message from an error object.
- * This function centralizes error message generation using the dbErrorHandler utility.
- 
- * errMsg - The error object.
- */
-
-function getErrorMessage(errMsg) {
-  const errorMessage = dbErrorHandler.getErrorMessage(errMsg);
-  //console.log("Error:", errorMessage); // Log the error for debugging
+/**
+* Generates a formatted error message from an error object.
+* This function centralizes error message generation using the dbErrorHandler utility.
+* @param err The error object.
+* @returns An appropriate error message. 
+*/
+function getErrorMessage(err) {
+  const errorMessage = dbErrorHandler.getErrorMessage(err);
+  //console.log("Error:", errorMessage); 
   return errorMessage;
 }
-// Export the functions
+
 export default {
   handleError: handleError,
   getErrorMessage: getErrorMessage,

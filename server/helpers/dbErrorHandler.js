d595bab68e9e500db69b7f4743aa53850ff6c472
Quinn <89018063+kqfall1@users.noreply.github.com>
2025-11-08 22:09:52 -0500

    Altered dbErrorHandler.js
    Added documentation. Also removed 11001 from the switch block, since it's legacy.

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -1,33 +1,52 @@
+/**
+* Gets an appropriate error message for all database errors. 
+* @param err A MongoDB error. 
+* @returns A string with appropriate output.
+*/ 
 const getErrorMessage = (err) => {
   let message = "";
+  
   if (err.code) {
     switch (err.code) {
+      /** MongoDB duplicate key error. */  
       case 11000:
-      case 11001:
         message = getUniqueErrorMessage(err);
         break;
       default:
         message = "Something went wrong";
     }
-  } else {
+  }
+  /** If err.code isn't present, the error is likely a Mongoose validation error. */  
+  else {
     for (let errName in err.errors) {
       if (err.errors[errName].message) message = err.errors[errName].message;
     }
   }
+  
   return message;
 };
 
+/**
+* Gets an error message for duplicate key errors. Used for errors with codes of 11000 only. 
+* This method should perhaps be refactored in the future to reduce brittleness. 
+* @param err A MongoDB error.
+* @returns A string with appropriate output.  
+*/
 const getUniqueErrorMessage = (err) => {
   let output;
+  
   try {
     let fieldName = err.message.substring(
       err.message.lastIndexOf(".$") + 2,
       err.message.lastIndexOf("_1")
     );
+    
     output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + " already exists";
-  } catch (ex) {
+  } 
+  catch (ex) {
     output = "Unique field already exists";
   }
+  
   return output;
 };
 

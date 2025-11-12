cad4d3c5010718bc79cb0842361d8a6fae58c989
Jackie <wzhou60@my.centennialcollege.ca>
2025-11-12 11:04:18 -0500

    proper mongo URI added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,20 @@
-import express from "express"
+import config from "../config.js";
+import app from "express.js";
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(config.mongoUri, {
+    //useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useUnifiedTopology: true
+    dbName:""
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  });
+mongoose.connection.on("error", () => {
+  throw new Error(`unable to connect to database: ${config.mongoUri}`);
+});
 
 let app = express()
 

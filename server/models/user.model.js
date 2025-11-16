b999d6c0e44434264891672007f91fa3180bfc26
Jackie <wzhou60@my.centennialcollege.ca>
2025-11-16 16:00:26 -0500

    add role to user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,35 +1,44 @@
 import mongoose from "mongoose";
 import crypto from "crypto";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Name is required",
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Name is required",
+    },
 
-  email: {
-    type: String,
-    trim: true,
-    unique: "Email already exists",
-    match: [/.+\@.+\..+/, "Please fill a valid email address"],
-    required: "Email is required",
-  },
+    email: {
+      type: String,
+      trim: true,
+      unique: "Email already exists",
+      match: [/.+\@.+\..+/, "Please fill a valid email address"],
+      required: "Email is required",
+    },
 
-  created: {
-    type: Date,
-    default: Date.now,
-  },
-  updated: Date,
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+    updated: Date,
 
-  hashed_password: {
-    type: String,
-    required: "Password is required",
-  },
-  salt: String,
-}, { timestamps: true });
+    hashed_password: {
+      type: String,
+      required: "Password is required",
+    },
+    salt: String,
 
-userSchema.virtual("password")
+    role: {
+      type: String,
+      default: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema
+  .virtual("password")
   .set(function (password) {
     this._password = password;
     this.salt = this.makeSalt();

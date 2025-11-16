56de884fbc6d79c1a0b76597c7c4d2f527d779c5
Quinn Keenan <kqfall1@gmail.com>
2025-11-16 16:33:45 -0500

    Added JS logic to a library in the client directory. The SignIn component is now functional.

--- config.js
+++ config.js
@@ -2,8 +2,6 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    /* "zlaanahmedwnrzr_db_user:6xhm4qaENmGJrFbu@cluster123.0fbdvtn.mongodb.net/Skeleton?retryWrites=true&w=majority&appName=Cluster123" ||
-    "mongodb+srv://jzhou39_db_user:KQHb9osqa8IBy5Nl@cluster0.svci2dc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" */
     mongoUri: 
         process.env.MONGODB_URI || 
         `mongodb+srv://kqfall1:Lp8uHYVnWZIq7M6X@com-github-kqfall1-book.naammbk.mongodb.net/` ||

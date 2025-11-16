62390a05053055dc5e48b1911646b40443591373
Quinn Keenan <kqfall1@gmail.com>
2025-11-16 16:42:56 -0500

    NavBar.jsx now renders dynamically based on the user session's state

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import auth from '../../lib/auth.helpers.js'
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { signOut } from '../../lib/api.auth.js'
+import { useEffect, useState } from 'react';
 import "./components.css";
 
 export default function NavBar({ cartCount = 0 }) {
-    return (
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [session, setSession] = useState(auth.isAuthenticated());
+
+    /** Updates the state of the component each time the route changes. */
+    useEffect(() => {setSession(auth.isAuthenticated())}, [location])
+
+    return (session ? 
+        <nav className="bs-nav">
+            <div className="bs-nav-left">
+                <Link to="/" className="bs-logo">Bookstream</Link>
+            </div>
+            <div className="bs-nav-right">
+                <a onClick={async (e) => {
+                    await signOut()
+                    setSession(auth.isAuthenticated())
+                    navigate('/')
+                }}>Sign Out</a>
+                <Link to="/cart" className="bs-link">
+                    Cart {cartCount > 0 && <span className="bs-cart-count">{cartCount}</span>}
+                </Link>
+            </div>
+        </nav>
+        :
         <nav className="bs-nav">
             <div className="bs-nav-left">
                 <Link to="/" className="bs-logo">Bookstream</Link>

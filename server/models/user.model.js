14c6b9f8d078e9cbef47a964e90d25682c42dacd
Jackie <wzhou60@my.centennialcollege.ca>
2025-11-18 14:14:02 -0500

    added salt to usr schma to fix sign in

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: "Password is required",
     },
+    salt: String,
   },
   { timestamps: true }
 );
@@ -29,9 +30,9 @@ userSchema
   .virtual("password")
   .set(function (password) {
     this._password = password;
-    //this.salt = this.makeSalt();
-    this.hashed_password = password;
-    //this.hashed_password = this.encryptPassword(password);
+    this.salt = this.makeSalt();
+    //this.hashed_password = password;
+    this.hashed_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password;

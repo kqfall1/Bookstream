308f94ba4bf956c966fbaf454076494cde00e9fc
Jackie <wzhou60@my.centennialcollege.ca>
2025-11-27 13:03:51 -0500

    cart controller and routes from Zlann

---
+++ server/routers/cart.routes.js
@@ -0,0 +1,19 @@
+import authCtrl from '../controllers/auth.controller.js'
+import cartCtrl from '../controllers/cart.controller.js'
+import express from 'express'
+
+const router = express.Router()
+
+router.route('/')
+    .get(authCtrl.requireSignin, cartCtrl.getCart)     
+    .post(authCtrl.requireSignin, cartCtrl.createCart)  
+router.route('/item')
+    .post(authCtrl.requireSignin, cartCtrl.addItem)     
+    .delete(authCtrl.requireSignin, cartCtrl.removeItem)
+
+router.route('/clear')
+    .post(authCtrl.requireSignin, cartCtrl.clearCart)    
+
+router.route('/checkout')
+    .post(authCtrl.requireSignin, cartCtrl.checkout)     
+export default router

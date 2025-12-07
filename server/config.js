ed6a5be1a6433f79d1137144392a6371c7c798a2
Quinn Keenan <kqfall1@gmail.com>
2025-12-07 15:55:32 -0500

    Removed server/.env.production and refactored server/config.js to simply call dotenv.config().

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
-import path from "path"
 import dotenv from "dotenv"
-dotenv.config({ path: path.resolve("server", `.env.${process.env.NODE_ENV || "development"}`)})
+
+dotenv.config()
 
 const config = {
   env: process.env.NODE_ENV || "development",

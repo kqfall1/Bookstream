6921b268c5902bb14853c33d80251876b34fb5fd
Quinn Keenan <kqfall1@gmail.com>
2025-12-07 19:26:05 -0500

    Final commit and push for Sprint 3.

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -55,10 +55,4 @@ cartSchema.pre("save", function () {
   );
 });
 
-/* function to get price
-cartSchema.pre("save", function (next) {
-  this.totalPrice = this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
-  next();
-}); */
-
 export default mongoose.model("Cart", cartSchema);
